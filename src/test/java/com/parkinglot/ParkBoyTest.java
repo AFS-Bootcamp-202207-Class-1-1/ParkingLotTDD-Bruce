abc556d2440b101d0aabe94097143d370643b843
darksheep <3297137947@qq.com>
2022-07-24 19:12:55 +0800

    test:should_return_right_car_with_each_ticket_when_fetch_twice_given_a_parkingBoy_with_two_parked_car_and_two_parking_ticket

--- src/test/java/com/parkinglot/ParkBoyTest.java
+++ src/test/java/com/parkinglot/ParkBoyTest.java
@@ -37,4 +37,21 @@ public class ParkBoyTest {
         // then
         assertEquals(car, fetchedCar);
     }
+    @Test
+    public void should_return_right_car_with_each_ticket_when_fetch_twice_given_a_parkingBoy_with_two_parked_car_and_two_parking_ticket() {
+        // given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car carA = new Car();
+        Car carB = new Car();
+        ParkingTicket parkingTicketA = parkingBoy.park(carA);
+        ParkingTicket parkingTicketB = parkingBoy.park(carB);
+
+        // when
+        Car firstFetchedCar = parkingBoy.fetch(parkingTicketA);
+        Car secondFetchedCar = parkingBoy.fetch(parkingTicketB);
+
+        // then
+        assertEquals(carA, firstFetchedCar);
+        assertEquals(carB, secondFetchedCar);
+    }
 }
